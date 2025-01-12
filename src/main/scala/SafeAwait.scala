ea0433a009f3c4be24ada8890fbd56989ce5853e
Charlie <charlesbthomas94@gmail.com>
2025-01-12 16:51:58 -0600

    safe await

---
+++ src/main/scala/SafeAwait.scala
@@ -0,0 +1,26 @@
+package dev.parvus
+
+import scala.concurrent.Future
+import ox.Ox
+import ox.supervised
+import ox.channels.*
+import ox.fork
+import ox.get
+import scala.util.Success
+import scala.util.Failure
+import ox.get
+
+extension [T](inline f: Future[T]) inline def await: T = awaitWithChannel(f)
+
+private def awaitWithChannel[T](f: => Future[T]): T = {
+  val channel = Channel.rendezvous[T]
+  f.onComplete {
+    case Success(value) =>
+      channel.send(value)
+      channel.done()
+    case Failure(exception) =>
+      channel.error(exception)
+      channel.done()
+  }
+  channel.receive()
+}
