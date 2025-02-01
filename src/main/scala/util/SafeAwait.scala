4d578924fd6cce16cc20c79a5280945b124983b2
charlesbthomas <charlie.thomas@machinify.com>
2025-02-01 14:46:27 -0600

    fix: whitespace

--- src/main/scala/util/SafeAwait.scala
+++ src/main/scala/util/SafeAwait.scala
@@ -11,13 +11,14 @@ import scala.util.Failure
 import ox.get
 import scala.concurrent.ExecutionContext
 
+given ExecutionContext = scala.concurrent.ExecutionContext.global
+
 extension [T](inline f: Future[T]) inline def await: T = awaitWithChannel(f)
 
 extension [T](inline f: Future[Option[T]])
   inline def awaitOpt: Option[T] = awaitWithChannel(f)
 
 private def awaitWithChannel[T](f: => Future[T]): T = supervised {
-  given ExecutionContext = scala.concurrent.ExecutionContext.global
   val channel = Channel.rendezvous[T]
   f.onComplete {
     case Success(value) =>
